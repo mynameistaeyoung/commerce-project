9a6ca771e3077b7786978043a1b15cbe2d73243c
mynameistaeyoung <cv4660@naver.com>
2024-09-01 23:19:34 +0900

    Main 페이지 라우트 추가#2

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Login from "../pages/login/Login"
 import Register from "../pages/register/Register"
-import App from "../App"
+import Main from "@/pages/main/Main"
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}/>
+        <Route path="/" element={<Main />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
       </Routes>
