056e043776d52eeffd52bbab1e4b772651c44789
mynameistaeyoung <cv4660@naver.com>
2024-09-04 14:58:23 +0900

    회원가입 페이지 db에 유저정보 보내기 완료 #5

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,87 +1,177 @@
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import { addDoc, collection } from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "@/firebase";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
+
 
 const Register = () => {
+
   const navigate = useNavigate();
 
-  const onClickCreateButton = () => {
-    navigate("/login")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+  const [date, setDate] = useState("")
+  const [number, setNumber] = useState("")
+  const [seller, setSeller] = useState(false)
+
+  const onChange = (event: any) => {
+    const {
+      target: { id, value },
+    } = event;
+    if (id === "user_create_email") {
+      setEmail(value)
+    }
+    if (id === "user_create_password") {
+      setPassword(value)
+    }
+    if (id === "user_create_name") {
+      setName(value)
+    }
+    if (id === "user_create_date") {
+      setDate(value)
+    }
+    if (id === "user_create_number") {
+      setNumber(value)
+    }
   }
+
+  const signUp = async (event: any) => {
+    event.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      const collectionRef = collection(db, "user")
+      const docRef = await addDoc(collectionRef, {
+        date: date,
+        email: email,
+        name: name,
+        number: number,
+        uid: user.uid,
+        seller: seller
+      });
+      console.log("Document written with ID: ", docRef.id);
+      navigate("/login");
+    } catch (error: any) {
+      console.log("에러 발생", error.message, error.code);
+      switch (error.code) {
+        case 'auth/invalid-email':
+          alert('이메일을 바르게 입력해주세요.')
+          break
+        case 'auth/weak-password':
+          alert('비밀번호가 너무 쉬워요.')
+          break
+        case 'auth/email-already-in-use':
+          alert('등록된 이메일 입니다.')
+          break
+        default:
+          alert('회원가입 실패')
+          break
+      };
+    }
+  };
+
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <form onSubmit={signUp} className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold mb-8">회원가입</h1>
       <div className="w-full max-w-2xl mx-auto">
-        {/* 아이디 */}
+        {/* 이메일 */}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="user_create_id">
-            아이디
-          </label>
+          <Label htmlFor="user_create_email">
+            이메일
+          </Label>
           <Input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
-            id="user_create_id"
+            id="user_create_email"
             type="text"
-            placeholder="사용하실 아이디를 입력해주세요!"
+            placeholder="이메일을 입력해주세요!"
+            value={email}
+            onChange={onChange}
           />
         </div>
 
         {/* 비밀번호 */}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="user_create_password">
+          <Label htmlFor="user_create_password">
             비밀번호
-          </label>
+          </Label>
           <Input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
             id="user_create_password"
             type="password"
             placeholder="사용하실 비밀번호 입력해주세요!"
+            value={password}
+            onChange={onChange}
           />
         </div>
 
         {/* 이름 */}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="user_name">
+          <Label htmlFor="user_name">
             이름
-          </label>
+          </Label>
           <Input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
-            id="user_name"
+            id="user_create_name"
             type="text"
             placeholder="성함을 입력해주세요!"
+            value={name}
+            onChange={onChange}
           />
         </div>
 
         {/* 생년월일 */}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="user_create_date">
+          <Label htmlFor="user_create_date">
             생년월일
-          </label>
+          </Label>
           <Input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
             id="user_create_date"
             type="text"
             placeholder="생년월일을 입력해주세요!"
+            value={date}
+            onChange={onChange}
           />
         </div>
 
         {/* 전화번호 */}
         <div className="mb-8">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="user_number">
+          <Label htmlFor="user_number">
             전화번호
-          </label>
+          </Label>
           <Input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
-            id="user_number"
+            id="user_create_number"
             type="text"
             placeholder="비밀번호를 입력해주세요!"
+            value={number}
+            onChange={onChange}
           />
         </div>
-        <button
-          className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          onClick={onClickCreateButton}>
+        <div>
+          <Checkbox
+            id="seller"
+            className="mb-5 mr-2"
+            value={seller}
+            onClick={() => { setSeller(!seller) }}
+          />
+          <Label htmlFor="seller">판매자입니까?</Label>
+        </div>
+        <Button
+          className="bg-black w-full mb-5"
+          type="submit">
           계정 생성하기
-        </button>
+        </Button>
+
+        <Button
+          className="bg-black w-full"
+          onClick={() => { navigate("/login") }}>
+          로그인하러가기
+        </Button>
       </div>
-    </div>
+    </form>
   )
 }
 
