73b3709008f89aa6e306ae4d63278a5d1b18f60e
mynameistaeyoung <cv4660@naver.com>
2024-09-06 23:15:25 +0900

    registration루트 추가#3

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Login from "../pages/login/Login"
 import Register from "../pages/register/Register"
 import Main from "@/pages/main/Main"
+import Mypage from "@/pages/mypage/Mypage"
+import Registration from "@/pages/registration/Registration"
 
 
 const Router = () => {
@@ -11,6 +13,8 @@ const Router = () => {
         <Route path="/" element={<Main />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
+        <Route path="/mypage" element={<Mypage />}/>
+        <Route path="/registration" element={<Registration />}/>
       </Routes>
     </BrowserRouter>
   )
