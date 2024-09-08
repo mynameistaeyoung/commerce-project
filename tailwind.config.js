b3c8f6b213f5a9d27f6c99dcfd8471e4bfd476ea
mynameistaeyoung <cv4660@naver.com>
2024-09-08 19:26:11 +0900

    aspectRatio tailwind 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      aspectRatio:{
+        '1/1':'1 / 1',
+        '16/9':'16 / 9'
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
