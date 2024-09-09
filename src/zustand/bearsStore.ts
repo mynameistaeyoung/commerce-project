a036d1848565c4f10ee2ec31fb75e7bd25084172
mynameistaeyoung <cv4660@naver.com>
2024-09-09 13:44:39 +0900

    goods,setGoods추가 #7

--- src/zustand/bearsStore.ts
+++ src/zustand/bearsStore.ts
@@ -9,14 +9,28 @@ interface User {
     seller: boolean;
 }
 
+export interface GoodsItem {
+    ProductDescription: string;
+    ProductName: string;
+    ProductPrice: string;
+    ProductURL: string;
+    UserUid: string | undefined;
+    ProductUid: string | undefined;
+}
+
 interface UserStore {
     user: User[]; // 수정된 부분
     setUser: (userData: User) => void;
+    goods: GoodsItem[];
+    setGoods: (goodsData: GoodsItem) => void
 }
+
 
 const useUserStore = create<UserStore>((set) => ({
     user: [],
+    goods: [],
     setUser: (userData: User) => set((state) => ({ user: [...state.user, userData] })),
+    setGoods: (goodsData: GoodsItem) => set((state) => ({ goods: [...state.goods, goodsData] }))
 }));
 
 export default useUserStore;
