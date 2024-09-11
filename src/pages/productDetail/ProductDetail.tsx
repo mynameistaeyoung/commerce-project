f9fc5f849445001775beea67c1c310d43d7f1ae4
mynameistaeyoung <cv4660@naver.com>
2024-09-11 21:43:30 +0900

    db에 있는 정보 삭제기능 추가 #4

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -3,6 +3,8 @@ import useUserStore from "@/zustand/bearsStore"
 import { useParams } from "react-router-dom"
 import { Button } from "@/components/ui/button"
 import { useNavigate } from "react-router-dom";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const ProductDetail = () => {
     const { goods } = useUserStore()
@@ -13,19 +15,34 @@ const ProductDetail = () => {
 
     const navigate = useNavigate()
 
+    const ProductDeleteButton = async (e: any) => {
+        e.preventDefault();
+        try {
+            if (confirm("정말 삭제하시겠습니까?") == true) {
+                await deleteDoc(doc(db, "goods", ProductUid!))
+            } else {
+                return;
+            }
+        } catch (error) {
+            console.log("삭제를 실패하였습니다", error)
+        }
+        navigate("/")
+    }
+
     return (
         <>
             <Header />
             <div className="w-[60%] mx-auto mt-[40px]">
                 {FoundGoods ? (<div className="flex">
-                    <img src={FoundGoods?.ProductURL} className="aspect-1/1 w-[60%] mr-4" />
+                    <img src={FoundGoods.ProductURL} className="aspect-1/1 w-[60%] mr-4" />
                     <div className="mt-[60px]">
                         <div className="font-bold text-3xl mb-10">{FoundGoods.ProductName}</div>
                         <div className="font-bold text-3xl">{FoundGoods.ProductPrice}</div>
                         <div className="font-bold text-3xl mb-10">{FoundGoods.ProductDescription}</div>
-                        <Button className="mr-3" onClick={() => navigate("/retouchProduct")}>수정하기</Button><Button>삭제하기</Button>
+                        <Button className="mr-3" onClick={() => navigate(`/retouchProduct/${ProductUid}`)}>수정하기</Button>
+                        <Button onClick={ProductDeleteButton}>삭제하기</Button>
                     </div>
-                </div>) : (alert("이미지를 불러올수 없습니다"))}
+                </div>) : (alert("상품정보를 불러올수 없습니다"))}
             </div>
         </>
     )
