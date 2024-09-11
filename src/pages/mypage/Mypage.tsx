92ef493c4e231cd0cad44072782118ae5714d50f
mynameistaeyoung <cv4660@naver.com>
2024-09-11 22:38:48 +0900

    유저정보 변경 기능 추가

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,17 +1,58 @@
 import Header from "@/components/header/Header"
 import useUserStore from "@/zustand/bearsStore"
-import { auth } from "../../firebase"
+import { auth, db } from "../../firebase"
 import { useState } from "react"
 import BgChangButton from '../../components/ui/BgChangeButton';
+import { Button } from "@/components/ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 
 const Mypage = () => {
-
     const userUid = auth.currentUser?.uid
-    const { user } = useUserStore();
+    const { user, updateUser } = useUserStore();
     const FoundUser = user.find(item => item.uid === userUid)
 
+    const [changeUserName, setChangeUserName] = useState(`${FoundUser?.name}`)
+    const [changeUserDate, setChangeUserDate] = useState(`${FoundUser?.date}`)
+    const [changeUserNumber, setChangeUserNumber] = useState(`${FoundUser?.number}`)
+    const [changeUserEmail, setChangeUserEmail] = useState(`${FoundUser?.email}`)
+    const [changeUserSeller, setChangeUserSeller] = useState(`${FoundUser?.seller}`)
+    const [changeUserUid, setChangeUserUid] = useState(`${FoundUser?.uid}`)
     const [activeMenu, setActiveMenu] = useState('내 정보');
     const menuItems = ['내 정보', '비밀번호 변경', '찜한항목', '장바구니'];
+
+    const navigate = useNavigate()
+
+    const changeUserProfileButton = async (e: any) => {
+        e.preventDefault();
+        try {
+            if (!userUid) return;
+            const changeUserRef = doc(db, "user", userUid)
+            await updateDoc(changeUserRef, {
+                name: changeUserName,
+                date: changeUserDate,
+                number: changeUserNumber,
+                uid: changeUserUid,
+                seller: changeUserSeller,
+                email: changeUserEmail
+            })
+            updateUser({
+                name: changeUserName,
+                date: changeUserDate,
+                number: changeUserNumber,
+                uid: changeUserUid,
+                seller: Boolean(changeUserSeller),
+                email: changeUserEmail
+            })
+            alert("유저정보가 변경되었습니다.")
+            navigate("/mypage")
+        } catch (error) {
+            console.log("정보를 바꾸는데 실패했습니다", error)
+        }
+
+    };
+
     return (
         <>
             <Header />
@@ -35,11 +76,31 @@ const Mypage = () => {
                         {FoundUser ? (
                             <div>
                                 <h3 className="text-xl mb-[20px] ml-[30px]">이메일 : {FoundUser.email}</h3>
-                                <h3 className="text-xl mb-[20px] ml-[30px]">이름 : {FoundUser.name}</h3>
-                                <h3 className="text-xl mb-[20px] ml-[30px]">전화번호 : {FoundUser.number}</h3>
-                                <h3 className="text-xl mb-[20px] ml-[30px]">생년월일 : {FoundUser.date}</h3>
+                                <div className="text-xl mb-[20px] ml-[30px]">
+                                    <label htmlFor="user-name">이름 : </label>
+                                    <input type="text" id="user-name"
+                                        value={changeUserName} onChange={(e) => { setChangeUserName(e.target.value) }} />
+                                </div>
+
+                                <div className="text-xl mb-[20px] ml-[30px]">
+                                    <label htmlFor="user-number">전화번호 : </label>
+                                    <input
+                                        type="text" id="user-number"
+                                        value={changeUserNumber} onChange={(e) => { setChangeUserNumber(e.target.value) }} />
+                                </div>
+
+                                <div className="text-xl mb-[20px] ml-[30px]">
+                                    <label htmlFor="user-date">생년월일 : </label>
+                                    <input
+                                        type="text" id="user-date"
+                                        value={changeUserDate} onChange={(e) => { setChangeUserDate(e.target.value) }} />
+                                </div>
                             </div>
+
                         ) : null}
+                        <div className="flex justify-end">
+                            <Button onClick={changeUserProfileButton}>수정하기</Button>
+                        </div>
                     </section>
                 </div>
             </div>
