768488a054de655dd0a9c47c892db8b1ae991426
mynameistaeyoung <cv4660@naver.com>
2024-09-11 13:40:18 +0900

    수정하기 페이지 및 디테일 페이지 라우트 추가 #2

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "../pages/login/Login"
 import Register from "../pages/register/Register"
 import Main from "@/pages/main/Main"
 import Mypage from "@/pages/mypage/Mypage"
 import Registration from "@/pages/registration/Registration"
+import ProductDetail from "@/pages/productDetail/ProductDetail"
+import RetouchProduct from "@/pages/retouchProduct/RetouchProduct"
 
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/mypage" element={<Mypage />}/>
-        <Route path="/registration" element={<Registration />}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/productDetail/:ProductUid" element={<ProductDetail />} />
+        <Route path="/retouchProduct" element={<RetouchProduct />} />
       </Routes>
     </BrowserRouter>
   )
