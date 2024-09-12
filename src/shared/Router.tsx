260302f28fbe2bca13f972e66c0dedc25cb3078a
mynameistaeyoung <cv4660@naver.com>
2024-09-12 22:53:39 +0900

    찜 목록 추가 #2

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -6,6 +6,7 @@ import Mypage from "@/pages/mypage/Mypage"
 import Registration from "@/pages/registration/Registration"
 import ProductDetail from "@/pages/productDetail/ProductDetail"
 import RetouchProduct from "@/pages/retouchProduct/RetouchProduct"
+import Like from "@/pages/mypage/Like"
 
 
 const Router = () => {
@@ -19,6 +20,7 @@ const Router = () => {
         <Route path="/registration" element={<Registration />} />
         <Route path="/productDetail/:ProductUid" element={<ProductDetail />} />
         <Route path="/retouchProduct/:ProductUid" element={<RetouchProduct />} />
+        <Route path="/like" element={<Like />} />
       </Routes>
     </BrowserRouter>
   )
