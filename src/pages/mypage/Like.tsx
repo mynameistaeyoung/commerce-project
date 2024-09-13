46f1425a6a38311cde0b37fbb5d43cde1c958b81
mynameistaeyoung <cv4660@naver.com>
2024-09-13 13:38:01 +0900

    찜한목록 컴포넌트 생성

--- src/pages/mypage/Like.tsx
+++ src/pages/mypage/Like.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import Header from "@/components/header/Header"
+import Collect from "./Collect"
+import { useEffect } from "react"
+import { db, auth } from "../../firebase"
+import { doc, getDoc } from "firebase/firestore";
+import { ProductLike } from "@/zustand/bearsStore";
+import useUserStore from "@/zustand/bearsStore";
 
 const Like = () => {
+  const userUid = auth.currentUser?.uid
+  const { like, setLike } = useUserStore()
+  console.log(like)
+
+  useEffect(() => {
+    const fetchLikeDate = async () => {
+      try {
+        const docRef = doc(db, "likes", userUid!)
+        const docSnap = await getDoc(docRef)
+        if (docSnap.exists()) {
+          const data = docSnap.data() as ProductLike
+          setLike(data)
+        }
+      } catch (error) {
+        console.log("에러발생", error)
+      }
+    }
+    fetchLikeDate()
+  }, [])
   return (
-    <div>Like</div>
+    <>
+      <Header />
+      <div className="w-[60%] mx-auto pt-6">
+        <div className="flex">
+          <Collect />
+          
+        </div>
+      </div>
+    </>
   )
 }
 
