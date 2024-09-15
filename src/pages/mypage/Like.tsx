d4767877ec6fd1062486ba2a142a03d9ed3f67ed
mynameistaeyoung <cv4660@naver.com>
2024-09-16 07:22:33 +0900

    찜한목록 컴포넌트 완료#3

--- src/pages/mypage/Like.tsx
+++ src/pages/mypage/Like.tsx
@@ -1,42 +1,64 @@
-import Header from "@/components/header/Header"
-import Collect from "./Collect"
-import { useEffect } from "react"
-import { db, auth } from "../../firebase"
-import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, auth } from "../../firebase";
 import { ProductLike } from "@/zustand/bearsStore";
 import useUserStore from "@/zustand/bearsStore";
+import { collection, onSnapshot } from "firebase/firestore"; 
+import { useNavigate } from "react-router-dom";
 
 const Like = () => {
-  const userUid = auth.currentUser?.uid
-  const { like, setLike } = useUserStore()
-  console.log(like)
+  const { setLike, goods } = useUserStore(); 
+  const [productLike, setProductLike] = useState<ProductLike[]>([]);
+  const [filteredGoods, setFilteredGoods] = useState<any[]>([]);
+  const userUid = auth.currentUser?.uid;
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchLikeDate = async () => {
+    const fetchLikeData = () => {
       try {
-        const docRef = doc(db, "likes", userUid!)
-        const docSnap = await getDoc(docRef)
-        if (docSnap.exists()) {
-          const data = docSnap.data() as ProductLike
-          setLike(data)
-        }
+        const unsubscribe = onSnapshot(collection(db, "likes"), (snapshot) => {
+          const likesArr: ProductLike[] = [];
+          snapshot.forEach((doc) => {
+            const data = doc.data() as ProductLike;
+            likesArr.push(data);
+            setLike(data);
+          });
+          setProductLike(likesArr);
+        });
+        return () => unsubscribe();
       } catch (error) {
-        console.log("에러발생", error)
+        console.log("에러발생", error);
       }
-    }
-    fetchLikeDate()
-  }, [])
-  return (
-    <>
-      <Header />
-      <div className="w-[60%] mx-auto pt-6">
-        <div className="flex">
-          <Collect />
-          
-        </div>
-      </div>
-    </>
-  )
-}
+    };
+    fetchLikeData();
+  }, [setLike]);
 
-export default Like
+  useEffect(() => {
+    const foundLike = productLike
+      .filter((item) => item.like === true && item.userUid === userUid)
+      .map((item) => item.ProductUid);
+
+    const matchedGoods = goods.filter((item) => foundLike.includes(item.ProductUid));
+    setFilteredGoods(matchedGoods);
+  }, [productLike, goods, userUid]);
+
+  return (
+    <section>
+      <h2 className="text-3xl mb-[30px] ml-[30px]">찜한항목</h2>
+      <ul className="w-[60%] ml-[30px] mt-[40px] grid grid-cols-4 gap-[30px]">
+        {filteredGoods.map((item) => (
+          <li
+            key={item.ProductUid}
+            className="font-bold cursor-pointer"
+            onClick={() => { navigate(`/productDetail/${item.ProductUid}`); }}
+          >
+            <img src={item.ProductURL} className="aspect-1/1" />
+            <p>{item.ProductName}</p>
+            <p>{item.ProductPrice}원</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Like;
