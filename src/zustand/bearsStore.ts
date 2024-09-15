da5f2fc7ff60e46a40ff57bd84b9512fe4dcfc38
mynameistaeyoung <cv4660@naver.com>
2024-09-16 08:52:20 +0900

    User export 추가 #7

--- src/zustand/bearsStore.ts
+++ src/zustand/bearsStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-interface User {
+export interface User {
     date: string;
     email: string;
     name: string;
