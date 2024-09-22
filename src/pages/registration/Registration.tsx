b71fe46bd4deab7362f284b2ed8d8e4cce4a1f80
mynameistaeyoung <cv4660@naver.com>
2024-09-23 01:28:43 +0900

    상품등록시 최대 상품수량 지정

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -21,6 +21,7 @@ const Registration = () => {
     const [showImages, setShowImages] = useState<string | ArrayBuffer | null>(null);
     const [uploading, setUploading] = useState<boolean>(false);
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [productQuantity, setProductQuantity] = useState(0)
 
     const navigate = useNavigate()
 
@@ -35,27 +36,33 @@ const Registration = () => {
         const productUuid = uuid();
         const storageRef = ref(storage, `images/${uuid()}`);
         try {
-            const snapshot = await uploadBytes(storageRef, selectedFile);
-            console.log("업로드 성공:", snapshot);
-            const uid = auth.currentUser?.uid
-            const downloadURL = await getDownloadURL(snapshot.ref);
-            await setDoc(doc(db, 'goods', productUuid), {
-                ProductDescription: productDescription,
-                ProductName: productName,
-                ProductPrice: productPrice,
-                ProductURL: downloadURL,
-                UserUid: uid,
-                ProductUid: productUuid
-            });
-            setGoods({
-                ProductDescription: productDescription,
-                ProductName: productName,
-                ProductPrice: productPrice,
-                ProductURL: downloadURL,
-                UserUid: uid,
-                ProductUid: productUuid
-            })
-            navigate("/");
+            if (productQuantity > 0) {
+                const snapshot = await uploadBytes(storageRef, selectedFile);
+                console.log("업로드 성공:", snapshot);
+                const uid = auth.currentUser?.uid
+                const downloadURL = await getDownloadURL(snapshot.ref);
+                await setDoc(doc(db, 'goods', productUuid), {
+                    ProductDescription: productDescription,
+                    ProductName: productName,
+                    ProductPrice: productPrice,
+                    ProductURL: downloadURL,
+                    UserUid: uid,
+                    ProductUid: productUuid,
+                    ProductQuantity: productQuantity
+                });
+                setGoods({
+                    ProductDescription: productDescription,
+                    ProductName: productName,
+                    ProductPrice: productPrice,
+                    ProductURL: downloadURL,
+                    UserUid: uid,
+                    ProductUid: productUuid,
+                    ProductQuantity: productQuantity
+                })
+                navigate("/");
+            }else{
+                alert("상품수량을 1개이상 등록하셔야 합니다.")
+            }
         } catch (error) {
             console.log("업로드 중 에러 발생:", error);
         } finally {
@@ -98,12 +105,12 @@ const Registration = () => {
                                 id="product-picture"
                                 type="file"
                                 accept="image/*"
-                                onChange={handleImageChange} // 이미지 변경 핸들러
+                                onChange={handleImageChange}
                             />
                         </div>
-                        {uploading && <p className="mt-2 text-black">이미지 업로드 중...</p>} {/* 업로드 상태 표시 */}
+                        {uploading && <p className="mt-2 text-black">이미지 업로드 중...</p>}
                     </div>
-                    <div className="flex-1">
+                    <div className="flex-1 flex flex-col gap-5 h-[450px]">
                         <div>
                             <Label htmlFor="product-name">상품명</Label>
                             <Input
@@ -111,30 +118,42 @@ const Registration = () => {
                                 type="text"
                                 id="product-name"
                                 value={productName}
-                                onChange={(e) => setProductName(e.target.value)} // onChange에서 직접 처리
+                                onChange={(e) => setProductName(e.target.value)}
                             />
                         </div>
                         <div>
                             <Label htmlFor="product-description">상품설명</Label>
-                            <div className="h-[282px] border border-gray-400 rounded-md">
+                            <div className="h-[150px] border border-gray-400 rounded-md">
                                 <input
-                                    className="border-0 focus:outline-none focus:ring-0 w-full"
+                                    className="border-0 focus:outline-none focus:ring-0 w-full h-full px-2 py-1"
                                     type="text"
                                     id="product-description"
                                     value={productDescription}
-                                    onChange={(e) => setProductDescription(e.target.value)} // onChange에서 직접 처리
+                                    onChange={(e) => setProductDescription(e.target.value)}
                                 />
                             </div>
                         </div>
-                        <div>
-                            <Label htmlFor="product-price">가격</Label>
-                            <Input
-                                className=" border-gray-400 mb-5"
-                                type="number"
-                                id="product-price"
-                                value={productPrice}
-                                onChange={(e) => setProductPrice(e.target.value)} // onChange에서 직접 처리
-                            />
+                        <div className="flex gap-5">
+                            <div className="flex-1">
+                                <Label htmlFor="product-price">가격</Label>
+                                <Input
+                                    className=" border-gray-400 mb-5"
+                                    type="number"
+                                    id="product-price"
+                                    value={productPrice}
+                                    onChange={(e) => setProductPrice(e.target.value)}
+                                />
+                            </div>
+                            <div className="flex-1">
+                                <Label htmlFor="product-quantity">상품수량</Label>
+                                <Input
+                                    className="border-gray-400 mb-5"
+                                    type="number"
+                                    id="product-quantity"
+                                    value={productQuantity}
+                                    onChange={(e) => { setProductQuantity(+e.target.value) }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
