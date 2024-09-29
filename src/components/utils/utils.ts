85064edc8f865164c2a64e55868ddc096da3014e
mynameistaeyoung <cv4660@naver.com>
2024-09-29 15:40:28 +0900

    addNewAddressToUser 및  fetchUserAddresses  공통 컴포넌트 생성

---
+++ src/components/utils/utils.ts
@@ -0,0 +1,30 @@
+import { doc, updateDoc, arrayUnion, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+// 공통 주소 추가 함수
+export const addNewAddressToUser = async (userUid: string, newAddress: string) => {
+    try {
+        const addressRef = doc(db, 'user', userUid);
+        await updateDoc(addressRef, {
+            addresses: arrayUnion(newAddress),
+        });
+        return true;
+    } catch (error) {
+        console.error("주소 추가에 실패했습니다:", error);
+        return false;
+    }
+};
+
+export const fetchUserAddresses = async (userUid: string) => {
+    try {
+        const docRef = doc(db, 'user', userUid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            return docSnap.data().addresses || [];
+        }
+    } catch (error) {
+        console.error("주소 목록을 불러오는 중 오류 발생:", error);
+    }
+    return [];
+};
